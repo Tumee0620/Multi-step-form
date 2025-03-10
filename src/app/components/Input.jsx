8726a6c623e7fec7f9b0fd92aa7cdb2e4f3c4812
Tumee0620 <ttumee0620@gmail.com>
2025-03-10 12:44:32 +0800

    multidesign

---
+++ src/app/components/Input.jsx
@@ -0,0 +1,9 @@
+export const Input = () => {
+  return (
+    <input
+      className="w-full  flex p-3 items-center text-[] rounded-[8px] border-[1px] border-solid border-[#0CA5E9] text-[#8B8E95] "
+      type="text"
+      placeholder="Placeholder"
+    />
+  );
+};
